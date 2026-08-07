de2ff2fbf428abb21f2dbba7bb28fe53842e4c08
ADMIN <admin@localhost>
2026-08-07 12:02:20 +0800

    Fix Electron login: disable webSecurity + allow Firebase domains via CSP session header

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain, Menu } = require('electron');
+const { app, BrowserWindow, ipcMain, Menu, session } = require('electron');
 const path = require('path');
 
 // Remove the native menu bar entirely
@@ -21,8 +21,23 @@ function createWindow() {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
       nodeIntegration: false,
-      webSecurity: true
+      webSecurity: false   // allow file:// page to reach Firebase APIs
     }
+  });
+
+  // Allow Firebase auth/firestore/storage network requests
+  session.defaultSession.webRequest.onHeadersReceived(function(details, callback) {
+    callback({
+      responseHeaders: Object.assign({}, details.responseHeaders, {
+        'Content-Security-Policy': [
+          "default-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob: " +
+          "https://*.googleapis.com https://*.google.com https://*.firebaseio.com " +
+          "https://*.firebaseapp.com https://*.googleapis.com wss://*.firebaseio.com " +
+          "https://firebasestorage.googleapis.com https://identitytoolkit.googleapis.com " +
+          "https://securetoken.googleapis.com https://www.googleapis.com"
+        ]
+      })
+    });
   });
 
   win.loadFile('index.html');
